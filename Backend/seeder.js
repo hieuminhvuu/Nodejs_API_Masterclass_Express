77b4b2a9e404bc0118aaed063a3c7773f7b15e6a
Hieu Minh Vu <hieuminhvuu@AlienBookpro.local>
2023-02-24 19:43:45 +0300

    Bootcamp-User relationship

--- Backend/seeder.js
+++ Backend/seeder.js
@@ -7,9 +7,9 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config/config.env" });
 
 // Load models
-const BootcampSchema = require("./models/Bootcamp");
 const Bootcamp = require("./models/Bootcamp");
 const Course = require("./models/Course");
+const User = require("./models/User");
 
 // Connect to DB
 mongoose.set("strictQuery", false);
@@ -26,12 +26,16 @@ const bootcamps = JSON.parse(
 const courses = JSON.parse(
     fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
 );
+const users = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/users.json`, "utf-8")
+);
 
 // Import into DB
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
         await Course.create(courses);
+        await User.create(users);
         console.log("Data imported...".green.inverse);
         process.exit();
     } catch (err) {
@@ -44,6 +48,7 @@ const deleteData = async () => {
     try {
         await Bootcamp.deleteMany();
         await Course.deleteMany();
+        await User.deleteMany();
         console.log("Data destroyed...".red.inverse);
         process.exit();
     } catch (err) {
